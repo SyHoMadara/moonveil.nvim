fbfb8c0cbed8014602212cf49fd6068c109737ef
Hosein Sarafrazi <sh.sarafrazi80@gmail.com>
2025-09-14 12:40:07 +0330

    clean snacks (format)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,92 +1,232 @@
 Snacks = Snacks
 
 return {
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      bigfile      = { enabled = true },
-      dashboard    = { enabled = false },
-      explorer     = { enabled = true },
-      indent       = { enabled = true },
-      input        = { enabled = SyHoMadaraVim.minimal_mode == false },
-      notifier     = { enabled = SyHoMadaraVim.minimal_mode == false },
-      picker       = {
-        enabled = true,
-        explorer = {
-          opts = {
-            win = {
-              list = {
-                keys = {
-                  ["<c-]>"] = "explorer_cd",
-                }
-              }
-            }
-          }
-        },
-        previewers = {
-          git = {
-            native = true, -- use native (terminal) or Neovim for previewing git diffs and commits
-          },
-        },
-        win = {
-          -- input window
-          input = {
-            keys = {
-              ["<Esc>"] = { "close", mode = { "n", "i" } },
-              ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
-              ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
-              ["<c-b>"] = { "list_scroll_up", mode = { "i", "n" } },
-              ["<c-f>"] = { "list_scroll_down", mode = { "i", "n" } },
-            },
-          },
-        },
-      },
-      quickfile    = { enabled = false },
-      scroll       = { enabled = false },
-      statuscolumn = {
-        enabled = true,
-      },
-      words        = { enabled = false },
-    },
-    keys = {
-      -- ╭─────────────────────────────────────────────────────────╮
-      -- │ Lazygit                                                 │
-      -- ╰─────────────────────────────────────────────────────────╯
-      { "<leader>lg",  function() Snacks.lazygit() end,                                        desc = "Lazygit" },
-      { "<leader>lga", function() Snacks.lazygit.log() end,                                    desc = "Lazygit Log (cwd)" },
-      { "<leader>llc", function() Snacks.lazygit.log_file() end,                               desc = "Lazygit Current File History" },
-      -- ╭─────────────────────────────────────────────────────────╮
-      -- │ Zen                                                     │
-      -- ╰─────────────────────────────────────────────────────────╯
-      { "<leader>z",   function() Snacks.zen({ win = { width = 200 } }) end,                   desc = "Zen Mode" },
-      { "<leader>Z",   function() Snacks.zen.zoom() end,                                       desc = "Zoom Mode" },
-      -- ╭─────────────────────────────────────────────────────────╮
-      -- │ Picker                                                  │
-      -- ╰─────────────────────────────────────────────────────────╯
-      { "<C-n>",       function() Snacks.picker.explorer() end,                                desc = "explorer" },
-      { "<leader>ff",       function() Snacks.picker.smart() end,                                   desc = "smart files" },
-      { "<leader>fw",       function() Snacks.picker.grep() end,                                    desc = "grep" },
-      { "<leader>fs",  function() Snacks.picker.grep_word() end,                               desc = "grep selected",                   mode = { "n", "v" } },
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			bigfile = { enabled = true },
+			dashboard = { enabled = false },
+			explorer = { enabled = true },
+			indent = { enabled = true },
+			input = { enabled = SyHoMadaraVim.minimal_mode == false },
+			notifier = { enabled = SyHoMadaraVim.minimal_mode == false },
+			picker = {
+				enabled = true,
+				explorer = {
+					opts = {
+						win = {
+							list = {
+								keys = {
+									["<c-]>"] = "explorer_cd",
+								},
+							},
+						},
+					},
+				},
+				previewers = {
+					git = {
+						native = true, -- use native (terminal) or Neovim for previewing git diffs and commits
+					},
+				},
+				win = {
+					-- input window
+					input = {
+						keys = {
+							["<Esc>"] = { "close", mode = { "n", "i" } },
+							["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
+							["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
+							["<c-b>"] = { "list_scroll_up", mode = { "i", "n" } },
+							["<c-f>"] = { "list_scroll_down", mode = { "i", "n" } },
+						},
+					},
+				},
+			},
+			quickfile = { enabled = false },
+			scroll = { enabled = false },
+			statuscolumn = {
+				enabled = true,
+			},
+			words = { enabled = false },
+		},
+		keys = {
+			-- ╭─────────────────────────────────────────────────────────╮
+			-- │ Lazygit                                                 │
+			-- ╰─────────────────────────────────────────────────────────╯
+			{
+				"<leader>lg",
+				function()
+					Snacks.lazygit()
+				end,
+				desc = "Lazygit",
+			},
+			{
+				"<leader>lga",
+				function()
+					Snacks.lazygit.log()
+				end,
+				desc = "Lazygit Log (cwd)",
+			},
+			{
+				"<leader>llc",
+				function()
+					Snacks.lazygit.log_file()
+				end,
+				desc = "Lazygit Current File History",
+			},
+			-- ╭─────────────────────────────────────────────────────────╮
+			-- │ Zen                                                     │
+			-- ╰─────────────────────────────────────────────────────────╯
+			{
+				"<leader>z",
+				function()
+					Snacks.zen({ win = { width = 200 } })
+				end,
+				desc = "Zen Mode",
+			},
+			{
+				"<leader>Z",
+				function()
+					Snacks.zen.zoom()
+				end,
+				desc = "Zoom Mode",
+			},
+			-- ╭─────────────────────────────────────────────────────────╮
+			-- │ Picker                                                  │
+			-- ╰─────────────────────────────────────────────────────────╯
+			{
+				"<A-n>",
 
-      { "<leader>pl",  function() Snacks.picker.projects() end,                                desc = "projects list" },
+				function()
+					Snacks.picker.explorer()
+				end,
+				desc = "explorer",
+			},
+			{
+				"<leader>ff",
+				function()
+					Snacks.picker.smart()
+				end,
+				desc = "smart files",
+			},
+			{
+				"<leader>fw",
+				function()
+					Snacks.picker.grep()
+				end,
+				desc = "grep",
+			},
+			{
+				"<leader>fs",
+				function()
+					Snacks.picker.grep_word()
+				end,
+				desc = "grep selected",
+				mode = { "n", "v" },
+			},
 
-      { "<leader>cd",  function() Snacks.picker.diagnostics() end,                             desc = "diagnostics" },
+			{
+				"<leader>pl",
+				function()
+					Snacks.picker.projects()
+				end,
+				desc = "projects list",
+			},
 
-      { "<leader>sf",  function() Snacks.picker.files() end,                                   desc = "files" },
-      { "<leader>sb",  function() Snacks.picker.buffers() end,                                 desc = "buffers" },
-      { "<leader>sh",  function() Snacks.picker.recent() end,                                  desc = "recent files" },
-      { "<leader>sH",  function() Snacks.picker.command_history() end,                         desc = "command history" },
-      { "<leader>ss",  function() Snacks.picker.search_history() end,                          desc = "search history" },
-      { "<leader>sq",  function() Snacks.picker.qflist() end,                                  desc = "quickfix list" },
-      { "<leader>sc",  function() Snacks.picker.colorschemes() end,                            desc = "color schemes" },
-      { "<leader>sd",  function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "dotfiles" },
+			{
+				"<leader>cd",
+				function()
+					Snacks.picker.diagnostics()
+				end,
+				desc = "diagnostics",
+			},
 
-      { "<leader>gf",  function() Snacks.picker.git_files() end,                               desc = "git files" },
-      { "<leader>gs",  function() Snacks.picker.git_status() end,                              desc = "git status" },
-      { "<leader>glA", function() Snacks.picker.git_log() end,                                 desc = "log" },
-      { "<leader>glC", function() Snacks.picker.git_log_file() end,                            desc = "file commits" },
-    },
-  }
+			{
+				"<leader>sf",
+				function()
+					Snacks.picker.files()
+				end,
+				desc = "files",
+			},
+			{
+				"<leader>sb",
+				function()
+					Snacks.picker.buffers()
+				end,
+				desc = "buffers",
+			},
+			{
+				"<leader>sh",
+				function()
+					Snacks.picker.recent()
+				end,
+				desc = "recent files",
+			},
+			{
+				"<leader>sH",
+				function()
+					Snacks.picker.command_history()
+				end,
+				desc = "command history",
+			},
+			{
+				"<leader>ss",
+				function()
+					Snacks.picker.search_history()
+				end,
+				desc = "search history",
+			},
+			{
+				"<leader>sq",
+				function()
+					Snacks.picker.qflist()
+				end,
+				desc = "quickfix list",
+			},
+			{
+				"<leader>sc",
+				function()
+					Snacks.picker.colorschemes()
+				end,
+				desc = "color schemes",
+			},
+			{
+				"<leader>sd",
+				function()
+					Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+				end,
+				desc = "dotfiles",
+			},
+
+			{
+				"<leader>gf",
+				function()
+					Snacks.picker.git_files()
+				end,
+				desc = "git files",
+			},
+			{
+				"<leader>gs",
+				function()
+					Snacks.picker.git_status()
+				end,
+				desc = "git status",
+			},
+			{
+				"<leader>glA",
+				function()
+					Snacks.picker.git_log()
+				end,
+				desc = "log",
+			},
+			{
+				"<leader>glC",
+				function()
+					Snacks.picker.git_log_file()
+				end,
+				desc = "file commits",
+			},
+		},
+	},
 }
