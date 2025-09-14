7eaf9b70fe85d19bb5576d08e48e3089679c934f
Hosein Sarafrazi <sh.sarafrazi80@gmail.com>
2025-09-14 12:37:14 +0330

    update icons

--- lua/config/SyHoMadaraVim.lua
+++ lua/config/SyHoMadaraVim.lua
@@ -8,7 +8,8 @@ SyHoMadaraVim = {
       border = "rounded", -- defutlt=rounded, none, single, double, solid, shadow
     }
   },
-  
+  minimal_mode = false, -- if true, disable some ui features
+  icons = icons, -- icons table 
   leader_key = ",", -- defutlt leader key is ","
 }
 
