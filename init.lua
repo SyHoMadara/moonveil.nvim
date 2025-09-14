621c7ef9c3f1438ce03c2ecc9ea0ecfb6d9acd4d
Hosein Sarafrazi <sh.sarafrazi80@gmail.com>
2025-09-14 09:21:35 +0330

    basic config

---
+++ init.lua
@@ -0,0 +1,3 @@
+require("config.SyHoMadaraVim")
+
+require("config.lazy")
---
+++ lua/config/SyHoMadaraVim.lua
@@ -0,0 +1,15 @@
+local icons = require("utils.icons")
+
+SyHoMadaraVim = {
+  -- General
+  colorscheme = "tokyonight", -- colorscheme name
+  ui = {
+    float = {
+      border = "rounded", -- defutlt=rounded, none, single, double, solid, shadow
+    }
+  },
+  
+  leader_key = ",", -- defutlt leader key is ","
+}
+
+return SyHoMadaraVim
