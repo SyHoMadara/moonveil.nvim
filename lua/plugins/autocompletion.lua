7a32dbea91692ef32d31943aeee22b5d641e65de
Hosein Sarafrazi <sh.sarafrazi80@gmail.com>
2025-09-21 07:54:27 +0330

    fix auto tag

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -231,21 +231,9 @@ return {
 		config = function()
 			require("nvim-ts-autotag").setup({
 				opts = {
-					enable_close = true, -- Auto close tags
+					enable_close = false, -- Auto close tags
 					enable_rename = true, -- Auto rename pairs of tags
 					enable_close_on_slash = true, -- Auto close on trailing </
-				},
-				-- ADD THIS LINE to specify the file types
-				filetypes = {
-					"html",
-					"javascript",
-					"typescript",
-					"javascriptreact",
-					"typescriptreact",
-					"svelte",
-					"vue",
-					"jsx",
-					"tsx",
 				},
 			})
 		end,
--- lua/plugins/parsers.lua
+++ lua/plugins/parsers.lua
@@ -30,7 +30,7 @@ return {
 					-- disable = { "c", "rust" },  -- list of language that will be disabled
 					-- additional_vim_regex_highlighting = false,
 				},
-
+        -- autotag = { enable = true },
 				folding = true,
 				incremental_selection = {
 					enable = false,
@@ -45,59 +45,6 @@ return {
 				indent = {
 					enable = true,
 				},
-
-				-- textobjects = {
-				-- 	move = {
-				-- 		enable = true,
-				-- 		set_jumps = true, -- whether to set jumps in the jumplist
-				-- 		goto_next_start = {
-				-- 			["]]"] = "@jsx.element",
-				-- 			["]f"] = "@function.outer",
-				-- 			["]m"] = "@class.outer",
-				-- 		},
-				-- 		goto_next_end = {
-				-- 			["]F"] = "@function.outer",
-				-- 			["]M"] = "@class.outer",
-				-- 		},
-				-- 		goto_previous_start = {
-				-- 			["[["] = "@jsx.element",
-				-- 			["[f"] = "@function.outer",
-				-- 			["[m"] = "@class.outer",
-				-- 		},
-				-- 		goto_previous_end = {
-				-- 			["[F"] = "@function.outer",
-				-- 			["[M"] = "@class.outer",
-				-- 		},
-				-- 	},
-				-- 	select = {
-				-- 		enable = true,
-				--
-				-- 		-- Automatically jump forward to textobj, similar to targets.vim
-				-- 		lookahead = true,
-				--
-				-- 		keymaps = {
-				-- 			-- You can use the capture groups defined in textobjects.scm
-				-- 			["af"] = "@function.outer",
-				-- 			["if"] = "@function.inner",
-				-- 			["ac"] = "@class.outer",
-				-- 			["ic"] = "@class.inner",
-				-- 		},
-				-- 	},
-				-- 	swap = {
-				-- 		enable = true,
-				-- 		swap_next = {
-				-- 			["~"] = "@parameter.inner",
-				-- 		},
-				-- 	},
-				-- },
-
-				-- textsubjects = {
-				-- 	enable = true,
-				-- 	prev_selection = "<BS>",
-				-- 	keymaps = {
-				-- 		["<CR>"] = "textsubjects-smart", -- works in visual mode
-				-- 	},
-				-- },
 			})
 		end,
 		dependencies = {
@@ -108,27 +55,6 @@ return {
 		},
 	},
 
-	{
-		"windwp/nvim-ts-autotag",
-		event = "BufReadPre",
-		config = function()
-			require("nvim-ts-autotag").setup({
-				opts = {
-					enable_close = false, -- Auto close tags
-					enable_rename = true, -- Auto rename pairs of tags
-					enable_close_on_slash = true, -- Auto close on trailing </
-				},
-				-- Also override individual filetype configs, these take priority.
-				-- Empty by default, useful if one of the "opts" global settings
-				-- doesn't work well in a specific filetype
-				--[[ per_filetype = {
-            ["html"] = {
-              enable_close = false
-            }
-          } ]]
-			})
-		end,
-	},
 
 	{
 		"wurli/contextindent.nvim",
